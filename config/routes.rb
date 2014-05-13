a5555141aeb268956248175312fa4b02ae5ab77d
Spyder Monkee McHenry & Maureen Dugan <student@epicodus.com>
2014-05-13 16:45:42 -0700

    destroys AdminUser model, removes the devise_create_admin_users.rb, corrects typo in method call for authenticate_active_admin_user in admin config file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 HouseOfSound::Application.routes.draw do
   devise_for :users
-  devise_for :admin_users, ActiveAdmin::Devise.config
+
   ActiveAdmin.routes(self)
   resources :programs do
     collection do

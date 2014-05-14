cee10e7899b11e458a9a0fad4373fc16820ab719
Spyder Monkee McHenry & Maureen Dugan <student@epicodus.com>
2014-05-13 17:03:25 -0700

    adds code to app/admin/user.rb to setup the create user form in admin dashboard

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,6 +1,32 @@
 ActiveAdmin.register User do
 
-  
+  form do |f|
+    f.inputs "User Details" do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+      f.input :superadmin, :label => "Super Administrator"
+    end
+    f.actions
+  end
+
+  create_or_edit = Proc.new {
+    @user            = User.find_or_create_by_id(params[:id])
+    @user.superadmin = params[:user][:superadmin]
+    @user.attributes = params[:user].delete_if do |k, v|
+      (k == "superadmin") ||
+      (["password", "password_confirmation"].include?(k) && v.empty? && !@user.new_record?)
+    end
+    if @user.save
+      redirect_to :action => :show, :id => @user.id
+    else
+      render active_admin_template((@user.new_record? ? 'new' : 'edit') + '.html.erb')
+    end
+  }
+  member_action :create, :method => :post, &create_or_edit
+  member_action :update, :method => :put, &create_or_edit
+
+
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -13,5 +39,5 @@ ActiveAdmin.register User do
   #  permitted << :other if resource.something?
   #  permitted
   # end
-  
+
 end

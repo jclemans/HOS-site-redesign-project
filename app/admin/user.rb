c01b43f41c8200d8a31789fdac7f60beccdbbfff
Spyder Monkee McHenry & Maureen Dugan <student@epicodus.com>
2014-05-14 15:05:38 -0700

    add method to allow user updates without password

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,5 +1,7 @@
+require 'pry'
+
 ActiveAdmin.register User do
-  permit_params :email, :password, :password_confirmation,
+  permit_params :email, :password, :password_confirmation, :user,
     role_ids:[]
 
   index do
@@ -21,7 +23,21 @@ ActiveAdmin.register User do
     end
   end
 
-
+  controller do
+    def update
+      @user = User.find(params[:id])
+      if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+        @user.update_without_password(permitted_params[:user])
+      else
+        @user.update_attributes(params[:user])
+      end
+      if @user.errors.blank?
+        redirect_to admin_users_path, :notice => "User updated successfully."
+      else
+        render :edit
+      end
+    end
+  end
 
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters

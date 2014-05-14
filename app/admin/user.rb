3183892c2b6adb5a1bd8509bc8a703b8358cca2b
Spyder Monkee McHenry & Maureen Dugan <student@epicodus.com>
2014-05-14 11:19:05 -0700

    removes superadmin components from active admin user dashboard, updates the authenticate_active_admin method to use rolify roles

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -5,18 +5,12 @@ ActiveAdmin.register User do
       f.input :email
       f.input :password
       f.input :password_confirmation
-      f.input :superadmin, :label => "Super Administrator"
     end
     f.actions
   end
 
   create_or_edit = Proc.new {
     @user            = User.find_or_create_by_id(params[:id])
-    @user.superadmin = params[:user][:superadmin]
-    @user.attributes = params[:user].delete_if do |k, v|
-      (k == "superadmin") ||
-      (["password", "password_confirmation"].include?(k) && v.empty? && !@user.new_record?)
-    end
     if @user.save
       redirect_to :action => :show, :id => @user.id
     else

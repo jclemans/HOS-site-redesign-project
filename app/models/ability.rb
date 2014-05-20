53c1f819fcee0596d430f132990a38a0b9bdbfae
Bonnie Bright <bonniebright@gmail.com>
2014-05-19 17:40:00 -0700

    setup CanCanCan role management

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,10 +5,11 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
     user ||= User.new # guest user (not logged in)
-      if user.admin?
+      if user.has_role? :Admin
         can :manage, :all
-    # elsif user.dj?
-    #    can :manage, "own program"
+      elsif user.has_role? :DJ
+       can [:update, :edit], User
+       can :read, :all
       else
         can :read, :all
       end

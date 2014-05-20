e8147a7f317984f0d3ebecd1f6217ea69c93d53e
Jeremy Clemans & Alex Kipling <bonniebright@gmail.com>
2014-05-20 09:16:10 -0700

    add duration to programs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140519234450) do
+ActiveRecord::Schema.define(version: 20140520000011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20140519234450) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.integer  "duration"
   end
 
   create_table "roles", force: true do |t|

49302848c42b3ac27b10a311441c0038dd437a67
Moe & Marty <student@epicodus.com>
2014-05-22 12:08:58 -0700

    prep for migration

--- lib/user_program_updater.rb
+++ lib/user_program_updater.rb
@@ -1,8 +1,6 @@
-require 'pry'
-
 class UserProgramUpdater
 	def self.run_all
-		Program.all.each do |p| 
+		Program.all.each do |p|
 			dj = User.where(djname: p.deejays).last
 			update_program dj, p
 		end
@@ -12,14 +10,13 @@ class UserProgramUpdater
 		program.update_attribute :user_id, dj.id
 		dj.add_role "DJ"
 	rescue
-		if program.email.blank? 
+		if program.email.blank?
 			email = program.id
 		else
 			email = program.email
 		end
-		dj = User.create(djname: program.deejays, email: email, password: "password", password_confirmation: "password", phone: program.deejays, name: program.deejays)	
+		dj = User.create(djname: program.deejays, email: email, password: "password", password_confirmation: "password", phone: program.deejays, name: program.deejays)
 		program.update_attribute :user_id, dj.id
-		dj.add_role "DJ"	
+		dj.add_role "DJ"
 	end
-	
 end

87c488f6d0b2d61d993e46f23e0f772e5d49fa05
Marty Fajardo & Jeremy Clemans <student@epicodus.com>
2014-05-23 16:55:53 -0700

    create validation method for JUST the start time of a scheduling, there is no reference to the day of the week

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -5,9 +5,14 @@ describe Schedule do
   it { should belong_to :program }
 
   it 'should not allow a scheduled event to be created if an existing scheduled event has a duration overlapping the new start time' do
-    program1 = FactoryGirl.build(:schedule)
-    program1.save(schedule_params)
-    program2 = FactoryGirl.build(:schedule)
-    program2.save(schedule_params).should eq(false)
+    scheduled_event1 = Schedule.create(:start_time => "12:00", :duration => 90, :program_id => 7)
+    scheduled_event2 = Schedule.new(:start_time => "13:00", :duration => 60, :program_id => 5)
+    scheduled_event2.save.should eq(false)
   end
+  it 'should allow a scheduled event to be created if an existing scheduled event has a duration overlapping the new start time' do
+
+    scheduled_event3 = Schedule.new(:start_time => "15:00", :duration => 60, :program_id => 5)
+    scheduled_event3.save.should eq(true)
+  end
+
 end

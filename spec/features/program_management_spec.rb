a55a27676f33397752933c958bd4b001c8fa73b1
Jeremy, Moe & Marty <student@epicodus.com>
2014-05-22 17:02:55 -0700

    simplify Program factory and add sign in and update program spec tests

--- spec/features/program_management_spec.rb
+++ spec/features/program_management_spec.rb
@@ -4,6 +4,7 @@ feature 'dj signs in and edits a program' do
 
   scenario 'dj signs in and goes to the program page' do
     FactoryGirl.create(:user, email: "deejay@email.com", password: "password")
+
     visit '/users/sign_in'
     fill_in "Email", with: "deejay@email.com"
     fill_in "Password", with: "password"
@@ -11,7 +12,11 @@ feature 'dj signs in and edits a program' do
     page.should have_content "Signed in successfully."
   end
 
-
-
-
+  scenario 'dj visits the program edit page and updates the program name.' do
+    FactoryGirl.create(:program, id: '99')
+    sign_in(FactoryGirl.create(:dj, email: "deejay@email.com", password: "password"))
+    visit '/programs/99/edit'
+    click_button 'Update'
+    page.should have_content 'Update successful.'
+  end
 end

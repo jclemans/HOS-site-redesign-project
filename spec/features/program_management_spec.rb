f860d2c07fae03a2a72bd9661d06c4e4839866c6
Moe, Jeremy, Spyder <dev@devs-mbp.domain>
2014-05-23 12:04:57 -0700

    --amend

--- spec/features/program_management_spec.rb
+++ spec/features/program_management_spec.rb
@@ -16,6 +16,24 @@ feature 'signed in admin manages programs' do
 		fill_in "Description", with: "Twitter is amazing, but be careful of these traps..."
 		select("Dustin", :from => "program[user_id]")
 		click_button "Create Program"
+		page.should have_content "DJ Dustin's Twitter Etiquette"
+	end
+
+	scenario 'admin edits a program' do
+		FactoryGirl.create(:dj, name: "Dustin")
+		FactoryGirl.create(:program, id: 35)
+		click_link "Admin Dashboard"
+		click_link "Programs"
+		within("//tr[@id='program_35']") do 
+			click_link "Edit"
+		end
+		fill_in "Title", with: "New Title"
+		fill_in "Description", with: "Description is different"
+		select("Dustin", :from => "program[user_id]")
+		click_button "Update Program"
+		page.should have_content "New Title"
+		page.should have_content "Description is different"
+
 	end
 
 end

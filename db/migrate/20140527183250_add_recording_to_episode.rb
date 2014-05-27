488822a1a60301470fc3d52eba938e77e2867ba1
Moe & Tara & Spyder(via remote) <dev@devs-mbp.domain>
2014-05-27 12:44:04 -0700

    added recording_file_name to episode model

---
+++ db/migrate/20140527183250_add_recording_to_episode.rb
@@ -0,0 +1,9 @@
+class AddRecordingToEpisode < ActiveRecord::Migration
+   def self.up
+    add_attachment :episodes, :recording
+  end
+
+  def self.down
+    remove_attachment :episodes, :recording
+  end
+end

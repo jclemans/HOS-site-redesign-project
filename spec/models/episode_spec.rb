26b611de47e14dbc3869e84d03922bbf966cf747
Moe & Tara & Spyder(via remote) <dev@devs-mbp.domain>
2014-05-27 16:21:13 -0700

    set up streamripper for custom file path and wrote tests for it.

--- spec/models/episode_spec.rb
+++ spec/models/episode_spec.rb
@@ -4,12 +4,19 @@ describe Episode do
 	let(:stream_url){ ENV["stream_url"] }
 	let(:duration){ 5 }
 	let(:duration_seconds){ duration * 60 }
-	let(:episode_file_path){ ENV["episode_file_path"] }
+	let(:episode_file_directory){ ENV["episode_file_path"] }
 	it { should have_many :tracks }
 
 	it "takes program.duration and converts it to seconds and inserts that value into streamripper" do
 		episode = Episode.new(:recorded_at => Time.now, :record_time => duration)
-		episode.should_receive(:system).with("(streamripper #{stream_url} -A -l #{duration_seconds} -i -m 300 -a -d #{episode_file_path})")
+		episode.stub!(:file_name).and_return "1.mp3"
+		expected_file_path = "#{episode_file_directory}/1.mp3"
+		episode.should_receive(:system).with("streamripper #{stream_url} -A -l #{duration_seconds} -i -m 300 -a -d #{expected_file_path}")
 		episode.save
+	end
+
+	it "should know it's file path" do
+		episode = Episode.create(:recorded_at => Time.now, :record_time => duration)
+		episode.file_path.should == "#{episode_file_directory}/#{episode.id}.mp3"
 	end
 end

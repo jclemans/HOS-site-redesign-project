180bad50bde85f41c36609affae8e5f68da07375
Spyder & Moe <student@epicodus.com>
2014-05-27 11:53:09 -0700

    remove cruft, add program model tests

--- spec/models/program_spec.rb
+++ spec/models/program_spec.rb
@@ -2,4 +2,7 @@ require 'spec_helper'
 
 describe Program do
 	it { should belong_to :user }
+  it { should have_many :schedules }
+  it { should have_many :episodes }
+  it { should validate_presence_of :user_id }
 end

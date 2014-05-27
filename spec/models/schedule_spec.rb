131feeaadd605ace882d07d4d2bfc2361d91e99a
Spyder & Moe <student@epicodus.com>
2014-05-27 13:12:50 -0700

    edit schedule tests

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -4,15 +4,23 @@ describe Schedule do
 
   it { should belong_to :program }
 
-  it 'should not allow a scheduled event to be created if an existing scheduled event has a duration overlapping the new start time' do
-    scheduled_event1 = Schedule.create(:start_time => "12:00", :duration => 90, :program_id => 7)
-    scheduled_event2 = Schedule.new(:start_time => "13:00", :duration => 60, :program_id => 5)
-    scheduled_event2.save.should eq(false)
-  end
-  it 'should allow a scheduled event to be created if an existing scheduled event has a duration overlapping the new start time' do
+  describe 'validate schedule conflicts' do
+    it 'should not allow a program schedule to be created if it falls within an existing program schedule duration, within a single days 24 hour period' do
+      schedule1 = Schedule.create(:start_time => "5:00", :duration => 90, :program_id => 7, :days_of_week => ["Monday"])
+      schedule2 = Schedule.new(:start_time => "6:00", :duration => 60, :program_id => 5, :days_of_week => ["Monday"])
+      schedule2.save.should eq(false)
+    end
 
-    scheduled_event3 = Schedule.new(:start_time => "15:00", :duration => 60, :program_id => 5)
-    scheduled_event3.save.should eq(true)
-  end
+    it 'should allow a program schedule to be created if the schedule is available' do
+      schedule1 = Schedule.create(:start_time => "15:00", :duration => 60, :program_id => 5, :days_of_week => ["Monday"])
+      schedule2 = Schedule.new(:start_time => "15:00", :duration => 60, :program_id => 5, :days_of_week => ["Wednesday"])
+      schedule2.save.should eq(true)
+    end
 
+    it 'should not allow a program schedule to be created, if it falls within an existing program schedule duration from the previous day' do
+      schedule1 = Schedule.create(:start_time => "23:30", :duration => 120, :program_id => 9, :days_of_week => ["Monday"])
+      schedule2 = Schedule.new(:start_time => "00:15", :duration => 60, :program_id => 7, :days_of_week => ["Tuesday"])
+      schedule2.save.should eq(false)
+    end
+  end
 end

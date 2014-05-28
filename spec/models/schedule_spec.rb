568663f59ac3ff837d03e3f8f3b474dfc6e595e1
Spyder & Moe <student@epicodus.com>
2014-05-28 11:43:00 -0700

    add new method for checking schedule conflicts

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -4,25 +4,25 @@ describe Schedule do
 
   it { should belong_to :program }
 
-  # describe 'validate schedule conflicts' do
-  #   it 'should not allow a program schedule to be created if it falls within an existing program schedule duration, within a single days 24 hour period' do
-  #     schedule1 = Schedule.create(:start_time => "5:00", :duration => 90, :program_id => 7, :day_of_week => ["Monday"])
-  #     schedule2 = Schedule.new(:start_time => "6:00", :duration => 60, :program_id => 5, :day_of_week => ["Monday"])
-  #     schedule2.save.should eq(false)
-  #   end
+  describe 'validate schedule conflicts' do
+    it 'should not allow a program schedule to be created if it falls within an existing program schedule duration, within a single days 24 hour period' do
+      schedule1 = Schedule.create(:start_time => "5:00", :duration => 90, :program_id => 7, :day_of_week => 1)
+      schedule2 = Schedule.new(:start_time => "6:00", :duration => 60, :program_id => 5, :day_of_week => 1)
+      schedule2.save.should eq(false)
+    end
 
-  #   it 'should allow a program schedule to be created if the schedule is available' do
-  #     schedule1 = Schedule.create(:start_time => "15:00", :duration => 60, :program_id => 5, :day_of_week => ["Monday"])
-  #     schedule2 = Schedule.new(:start_time => "15:00", :duration => 60, :program_id => 5, :day_of_week => ["Wednesday"])
-  #     schedule2.save.should eq(true)
-  #   end
+    it 'should allow a program schedule to be created if the schedule is available' do
+      schedule1 = Schedule.create(:start_time => "15:00", :duration => 60, :program_id => 5, :day_of_week => 1)
+      schedule2 = Schedule.new(:start_time => "15:00", :duration => 60, :program_id => 5, :day_of_week => 3)
+      schedule2.save.should eq(true)
+    end
 
-  #   it 'should not allow a program schedule to be created, if it falls within an existing program schedule duration from the previous day' do
-  #     schedule1 = Schedule.create(:start_time => "23:30", :duration => 120, :program_id => 9, :day_of_week => ["Monday"])
-  #     schedule2 = Schedule.new(:start_time => "00:15", :duration => 60, :program_id => 7, :day_of_week => ["Tuesday"])
-  #     schedule2.save.should eq(false)
-  #   end
-  # end
+    it 'should not allow a program schedule to be created, if it falls within an existing program schedule duration from the previous day' do
+      schedule1 = Schedule.create(:start_time => "23:30", :duration => 120, :program_id => 9, :day_of_week => 1)
+      schedule2 = Schedule.new(:start_time => "00:00", :duration => 60, :program_id => 7, :day_of_week => 2)
+      schedule2.save.should eq(false)
+    end
+  end
 
   describe 'schedule with 3 segments' do
     let(:program){ FactoryGirl.build(:program) }

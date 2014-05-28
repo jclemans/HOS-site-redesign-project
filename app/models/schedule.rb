4d14b08c6c2f8ec5e530b376c6d470e13fc568e6
Spyder & Moe <student@epicodus.com>
2014-05-27 18:10:44 -0700

    create segment tests for schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -6,22 +6,41 @@ class Schedule < ActiveRecord::Base
   def schedule_conflicts
     new_start_time = self.start_time
     new_duration = self.duration.minutes
-    days = self.days_of_week
-    result = []
-
-    Schedule.all.each do |schedule|
-      schedule.days_of_week.each do |existing_schedule_day|
-        days.each do |new_schedule_day|
-          if existing_schedule_day == new_schedule_day && schedule.start_time <= new_start_time && new_start_time <= (schedule.start_time + schedule.duration.minutes)
-            result << schedule
-          end
-        end
-      end
-    end
-
-
-    if result.last
-      errors.add(:start_time, "Conflicts with program #{result.last.program_id}")
+    new_day = self.day_of_week
+    start_conflicts = checker(new_start_time, new_day)
+    if
+      errors.add(:start_time, "Conflicts with program #{start_conflicts.last.program_id}")
     end
   end
+
+  def checker(new_start_time, new_day)
+    result = []
+    Schedule.all.each do |schedule|
+      if schedule.day_of_week == new_day && schedule.start_time <= new_start_time && new_start_time <= (schedule.start_time + schedule.duration.minutes)
+        result << schedule
+      end
+    end
+    result
+  end
+
+  def segments
+    results = []
+    (0...self.duration).step(30) do |n|
+      segment_time = self.start_time + n.minutes
+      if segment_time.wday != self.start_time.wday
+        segment_day = (self.day_of_week + 1) % 7
+      else
+        segment_day = self.day_of_week
+      end
+      results << "#{segment_day}-#{segment_time.strftime('%H:%M')}"
+    end
+    results
+    # self.start_time
+  end
+
+  # schedules = Schedule.where(day_of_week:[(new_day-1)..(new_day + 1)])
+  # existing_segments = schedules.collect(&:segments).flatten
+  #  do an intersection of these two arrays to see if there is an overlap
 end
+
+
--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -4,23 +4,50 @@ describe Schedule do
 
   it { should belong_to :program }
 
-  describe 'validate schedule conflicts' do
-    it 'should not allow a program schedule to be created if it falls within an existing program schedule duration, within a single days 24 hour period' do
-      schedule1 = Schedule.create(:start_time => "5:00", :duration => 90, :program_id => 7, :days_of_week => ["Monday"])
-      schedule2 = Schedule.new(:start_time => "6:00", :duration => 60, :program_id => 5, :days_of_week => ["Monday"])
-      schedule2.save.should eq(false)
+  # describe 'validate schedule conflicts' do
+  #   it 'should not allow a program schedule to be created if it falls within an existing program schedule duration, within a single days 24 hour period' do
+  #     schedule1 = Schedule.create(:start_time => "5:00", :duration => 90, :program_id => 7, :day_of_week => ["Monday"])
+  #     schedule2 = Schedule.new(:start_time => "6:00", :duration => 60, :program_id => 5, :day_of_week => ["Monday"])
+  #     schedule2.save.should eq(false)
+  #   end
+
+  #   it 'should allow a program schedule to be created if the schedule is available' do
+  #     schedule1 = Schedule.create(:start_time => "15:00", :duration => 60, :program_id => 5, :day_of_week => ["Monday"])
+  #     schedule2 = Schedule.new(:start_time => "15:00", :duration => 60, :program_id => 5, :day_of_week => ["Wednesday"])
+  #     schedule2.save.should eq(true)
+  #   end
+
+  #   it 'should not allow a program schedule to be created, if it falls within an existing program schedule duration from the previous day' do
+  #     schedule1 = Schedule.create(:start_time => "23:30", :duration => 120, :program_id => 9, :day_of_week => ["Monday"])
+  #     schedule2 = Schedule.new(:start_time => "00:15", :duration => 60, :program_id => 7, :day_of_week => ["Tuesday"])
+  #     schedule2.save.should eq(false)
+  #   end
+  # end
+
+  describe 'schedule with 3 segments' do
+    let(:program){ FactoryGirl.build(:program) }
+    let(:schedule){ Schedule.new(start_time: "12:00", duration: 90, program: program, day_of_week: 2) }
+
+    it 'should include a start' do
+      schedule.segments.should include('2-12:00')
     end
 
-    it 'should allow a program schedule to be created if the schedule is available' do
-      schedule1 = Schedule.create(:start_time => "15:00", :duration => 60, :program_id => 5, :days_of_week => ["Monday"])
-      schedule2 = Schedule.new(:start_time => "15:00", :duration => 60, :program_id => 5, :days_of_week => ["Wednesday"])
-      schedule2.save.should eq(true)
+    it 'should include a middle segment' do
+      schedule.segments.should include('2-12:30')
     end
 
-    it 'should not allow a program schedule to be created, if it falls within an existing program schedule duration from the previous day' do
-      schedule1 = Schedule.create(:start_time => "23:30", :duration => 120, :program_id => 9, :days_of_week => ["Monday"])
-      schedule2 = Schedule.new(:start_time => "00:15", :duration => 60, :program_id => 7, :days_of_week => ["Tuesday"])
-      schedule2.save.should eq(false)
+    it 'should include an end segment' do
+      schedule.segments.should include('2-13:00')
     end
+
+    it 'should handle schedules spanning multiple days' do
+      test_schedule = Schedule.new(start_time: "23:30", duration: 90, program: program, day_of_week: 2)
+      test_schedule.segments.should include('3-00:30')
+    end
+    it 'should handle schedules spanning multiple weeks' do
+      test_schedule = Schedule.new(start_time: "23:30", duration: 90, program: program, day_of_week: 6)
+      test_schedule.segments.should include('0-00:30')
+    end
+
   end
 end

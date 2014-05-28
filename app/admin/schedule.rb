7c4703564e573c0878d9e99c87eb86e8616dbec1
Spyder & Moe <student@epicodus.com>
2014-05-28 14:53:06 -0700

    edit schedule in active admin

--- app/admin/schedule.rb
+++ app/admin/schedule.rb
@@ -1,5 +1,21 @@
 ActiveAdmin.register Schedule do
 
-  permit_params :program_id, :start_time, :duration, days_of_week: [], :episodes_attributes => [:Name, :recorded_at, :id, :_destroy]
+  permit_params :program_id, :start_time, :duration, :day_of_week, :episodes_attributes => [:Name, :recorded_at, :id, :_destroy]
 
+  actions :all, :except => [:new]
+  config.batch_actions = false
+
+  index  do
+    column :start_time
+    column :duration
+    column :day_of_week
+  end
+
+  show do |ad|
+    attributes_table do
+      row :start_time
+      row :duration
+      row :day_of_week
+    end
+  end
 end

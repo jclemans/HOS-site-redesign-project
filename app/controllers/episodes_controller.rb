88f38f6f91ddd26ffbe326805a75789d7c0967a9
Jeremy Clemans & Marty Fajardo <student@epicodus.com>
2014-05-29 15:33:19 -0700

    build and test episode/new for recording episode

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -5,11 +5,24 @@ class EpisodesController < ApplicationController
 
   def new
     @user = current_user
-    @episode = Episode.new
+    record_time = current_user.program.schedules.last.duration
+    program_id = current_user.program.id
+    @episode = Episode.new(record_time: record_time, program_id: program_id)
   end
 
   def create
-    @episode = Episode.create
-    redirect_to user_path(current_user)
+    if Episode.create(episode_params)
+      flash[:alert] = "Recording in progress"
+      redirect_to root_path
+    else
+      flash[:alert] = "Unable to record at this time. Please check the schedule."
+    end
+  end
+
+  private
+
+  def episode_params
+    params.require(:episode).permit(:recorded_at, :record_time,
+                                    :program_id, :title, :recording_file_name)
   end
 end
---
+++ spec/features/episode_management_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+feature 'DJ records an episode' do
+
+  before :each do
+    sign_in(FactoryGirl.create(:dj, id: 14))
+    FactoryGirl.create(:program, user_id: 14, id: 25)
+    Schedule.create(start_time: "07:00", duration: 120, program_id: 25, day_of_week: 6)
+  end
+
+  scenario 'DJ signs in to record a new episode' do
+    click_link "Record Episode"
+    click_button "Record Episode"
+    page.should have_content "Recording in progress"
+  end
+
+end

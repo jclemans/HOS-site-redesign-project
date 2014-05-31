73444072311c044a0b80091f7d3fa2a49b1473cc
Spyder & Alex & Maureen <student@epicodus.com>
2014-05-30 17:09:28 -0700

    created migration for getting day of week, start time,duration and program id from programs created in schedule model.

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,10 +1,15 @@
 class SchedulesController < ApplicationController
 
+  load_and_authorize_resource
+
   def index
-
+    @schedules = Schedule.all.inject({}) do |h, schedule|
+      h[schedule.day_of_week] ||= []
+      h[schedule.day_of_week] << schedule
+      h
+    end
   end
-
 
 end
 
-
+# h.sort_by { |k,v| v[:day_of_week] }

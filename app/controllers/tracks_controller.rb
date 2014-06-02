b33ef4ea4981617fddb78bf009adf1c476ef63b4
Jeremy Clemans & Marty Fajardo <student@epicodus.com>
2014-06-02 12:49:26 -0700

    adds functionality to add tracks to episodes

---
+++ app/controllers/tracks_controller.rb
@@ -0,0 +1,17 @@
+class TracksController < ApplicationController
+
+  def create
+    if Track.create(track_params)
+      flash[:alert] = "Track added."
+      redirect_to(:back)
+    else
+      flash[:alert] = "Oops, there was a problem. Try entering your track again."
+    end
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:episode_id, :content)
+  end
+end

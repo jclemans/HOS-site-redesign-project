0126bb2022b19989d89ef34665e46a725240b5d8
Moe & Tara <dev@devs-mbp.domain>
2014-06-02 15:15:05 -0700

    refactor navbar code and edit capybara tests for user management

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,17 @@
 class HomeController < ApplicationController
   def index
-        @key = "LKnK9JeMKEn5uRDHaZeNO4IUxkxiWOx8tNPeMcZl2IpfV8GLpU"
-        @secret = "u0mHkS1p3QxXCTBmt5LnCGDMQc8ZuNZ5VjFTq5wA7EvgvhvyYC"
-        @oauth_token = "UBvogCSj90wpvXXg53nYRWmGnFgbm1C08B8nXSuCvUa7uuID1c"
-        @oauth_token_secret = "bBP8LdTg1fLhrMS8BoELWn560EQpkhKwMpZGXbfxY1fC0UuYi1"
-        @myClient = Tumblr::Client.new(
-            :consumer_key => @key,
-            :consumer_secret => @secret,
-            :oauth_token => @oauth_token,
-            :oauth_token_secret => @oauth_token_secret
-        )
-        @posts = @myClient.posts("thehouseofsound.tumblr.com", :limit => 10)
-        @posts = @posts["posts"]
+        # @key = "LKnK9JeMKEn5uRDHaZeNO4IUxkxiWOx8tNPeMcZl2IpfV8GLpU"
+        # @secret = "u0mHkS1p3QxXCTBmt5LnCGDMQc8ZuNZ5VjFTq5wA7EvgvhvyYC"
+        # @oauth_token = "UBvogCSj90wpvXXg53nYRWmGnFgbm1C08B8nXSuCvUa7uuID1c"
+        # @oauth_token_secret = "bBP8LdTg1fLhrMS8BoELWn560EQpkhKwMpZGXbfxY1fC0UuYi1"
+        # @myClient = Tumblr::Client.new(
+        #     :consumer_key => @key,
+        #     :consumer_secret => @secret,
+        #     :oauth_token => @oauth_token,
+        #     :oauth_token_secret => @oauth_token_secret
+        # )
+        # @posts = @myClient.posts("thehouseofsound.tumblr.com", :limit => 10)
+        # @posts = @posts["posts"]
 
     # @user = Tumblog.find_by_user_id(5)
     # @client = Tumblr::Client.new(:consumer_key => @key, :consumer_secret => @secret, :oauth_token => @user.oauth_token, :oauth_token_secret => @user.oauth_secret)

2e3f4ae0828bb254b04fad62893c3bb2e73e1488
Jeremy Clemans & Maureen <dev@devs-MacBook-Pro-2.local>
2014-06-03 16:42:56 -0700

    adds a test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
 
-  def index
-    @schedules = Schedule.where(day_of_week: Date.today.wday)
-  end
-
   def listen_live
     render layout: 'bare'
   end

a30eef2b4d890aba250002b340150193ea77af3b
Spyder & Alex & Maureen <student@epicodus.com>
2014-06-02 17:50:24 -0700

    schedule display by duration amount in progress, created table and helper methods.

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,15 +1,15 @@
+require 'hos/schedule_helpers'
 class SchedulesController < ApplicationController
 
   load_and_authorize_resource
 
   def index
-    @schedules = Schedule.all.inject({}) do |h, schedule|
-      h[schedule.day_of_week] ||= []
-      h[schedule.day_of_week] << schedule
-      h
+    @schedules = Schedule.all.inject({}) do |days, schedule|
+      days[schedule.day_of_week] ||= []
+      days[schedule.day_of_week] << schedule
+      days
     end
   end
-
 end
 
 # h.sort_by { |k,v| v[:day_of_week] }

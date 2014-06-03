868e8fe0689cee73f646d6e360caf8414cf13948
Jeremy Clemans & Maureen <dev@devs-MacBook-Pro-2.local>
2014-06-03 16:42:56 -0700

    adds a test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,5 @@
 class HomeController < ApplicationController
 
-  def index
-    @schedules = Schedule.where(day_of_week: Date.today.wday)
-  end
-
-<<<<<<< HEAD
-  def index
-    @schedules = Schedule.where(day_of_week: Date.today.wday)
-=======
-  # def index
-  #   @schedules = Schedule.where(day_of_week: Date.today.wday)
-  # end
->>>>>>> rewrites and refactors the now playing functionality in two methods, appropriate tests and display
-
   def listen_live
     render layout: 'bare'
   end

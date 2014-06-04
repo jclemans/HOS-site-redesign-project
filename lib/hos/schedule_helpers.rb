f6a2a29a217e8540cb56e3199abbb1879e195809
Moe & Tara <dev@devs-MacBook-Pro.local>
2014-06-04 12:06:17 -0700

    removed the methods in scheduleshelpers that we didn't wind up using.

--- lib/hos/schedule_helpers.rb
+++ lib/hos/schedule_helpers.rb
@@ -5,17 +5,5 @@ module HOS
       days = { 'Sunday' => 0, 'Monday' => 1, 'Tuesday' => 2, 'Wednesday' => 3, 'Thursday' => 4, 'Friday' => 5, 'Saturday' => 6}
       days.key(number)
     end
-
-    def day_total(schedules)
-      durations =  schedules.collect do |schedule|
-        schedule.duration
-      end
-      durations.sum
-    end
-
-    def calc_schedule_percent(duration, schedules)
-      day_duration = day_total(schedules)
-      duration / day_duration * 100
-    end
   end
 end

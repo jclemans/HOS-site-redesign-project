d3f6ca3b2587ab4c2bb3876414b2faed7bff7549
Alex Kipling & Jeremy Clemans <jer.clemans@gmail.com>
2014-06-06 12:36:36 -0700

    rewrites and refactors the now playing functionality in two methods, appropriate tests and display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,23 +1,10 @@
 class HomeController < ApplicationController
-  def index
-    @schedules = Schedule.where(day_of_week: Date.today.wday)
-        # @key = "LKnK9JeMKEn5uRDHaZeNO4IUxkxiWOx8tNPeMcZl2IpfV8GLpU"
-        # @secret = "u0mHkS1p3QxXCTBmt5LnCGDMQc8ZuNZ5VjFTq5wA7EvgvhvyYC"
-        # @oauth_token = "UBvogCSj90wpvXXg53nYRWmGnFgbm1C08B8nXSuCvUa7uuID1c"
-        # @oauth_token_secret = "bBP8LdTg1fLhrMS8BoELWn560EQpkhKwMpZGXbfxY1fC0UuYi1"
-        # @myClient = Tumblr::Client.new(
-        #     :consumer_key => @key,
-        #     :consumer_secret => @secret,
-        #     :oauth_token => @oauth_token,
-        #     :oauth_token_secret => @oauth_token_secret
-        # )
-        # @posts = @myClient.posts("thehouseofsound.tumblr.com", :limit => 10)
-        # @posts = @posts["posts"]
+
+  # def index
+  #   @schedules = Schedule.where(day_of_week: Date.today.wday)
+  # end
 
   def listen_live
     render layout: 'bare'
-  end
-
-  def index
   end
 end

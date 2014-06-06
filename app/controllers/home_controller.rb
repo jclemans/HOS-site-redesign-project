74a8abdda48db37b490aa117e992e0b98aed59c8
Alex Kipling & Jeremy Clemans <jer.clemans@gmail.com>
2014-06-06 12:36:36 -0700

    rewrites and refactors the now playing functionality in two methods, appropriate tests and display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,10 @@
 class HomeController < ApplicationController
+
   def index
     @schedules = Schedule.where(day_of_week: Date.today.wday)
   end
 
   def listen_live
     render layout: 'bare'
-  end
-
-  def index
   end
 end

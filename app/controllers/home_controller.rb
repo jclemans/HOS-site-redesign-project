568d6f12edbc65e9b1b2ad04d387c9a51cce6d94
winescout <matt@notch8.com>
2014-06-12 19:13:32 -0700

    adds show time to next up show on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
     @now_playing = Schedule.now_playing(Time.now)
+    @now_playing = @now_playing.decorate if @now_playing
     @next_up = Schedule.find_next_schedule
+    @next_up = @next_up.decorate if @next_up
     render layout: 'home'
   end
 

336cd75a03025bd45dc7c1b8cfc6a0bd3b12a58f
winescout <matt@notch8.com>
2014-06-12 18:51:09 -0700

    fixes some ui issues, and links now playing to program page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @now_playing = Schedule.now_playing(Time.now)
+    @next_up = Schedule.find_next_schedule
     render layout: 'home'
   end
 
